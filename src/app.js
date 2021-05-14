83944fb7305c1d735600c85baf73fa99a5176a29
--replace-all <Andre>
2021-05-13 20:03:43 -0600

    Updates for IICO2021

--- src/app.js
+++ src/app.js
@@ -9,117 +9,178 @@ import './app.scss';
 function App() {
   let scroll = 0;
   let [meetings, setMeetings] = useState([
+    // Monday
     new Data_Model(
       'Wellness Club',
-      '10:00am – 11:00am',
-      'https://ulacit.zoom.us/j/96039347343',
+      '9:00am – 10:00am',
+      'https://ulacit.zoom.us/j/97982348200',
       null,
-      'wellness'
-    ),
-    new Data_Model(
-      'Debate Club',
-      '10:00am – 11:00am',
-      'https://ulacit.zoom.us/j/98925145035',
-      null,
-      'debate'
-    ),
-    new Data_Model(
-      'English Club',
-      '10:00am – 11:00am',
-      'https://ulacit.zoom.us/j/99941946188',
-      null,
-      'english'
-    ),
-    new Data_Model(
-      'Green Club',
-      '11:00am – 12:00pm',
-      'https://ulacit.zoom.us/j/92742460419',
-      null,
-      'green'
-    ),
-    new Data_Model(
-      'International Club',
-      '11:00am – 12:00pm',
-      'https://ulacit.zoom.us/j/97254839076',
-      null,
-      'international'
+      'wellness',
+      'lunes'
     ),
     new Data_Model(
       'Basketball Club',
-      '11:00am – 12:00pm',
-      'https://ulacit.zoom.us/j/96457613279',
+      '2:00pm – 3:00pm',
+      'https://ulacit.zoom.us/j/95790679751',
       null,
-      'sports'
+      'sports',
+      'lunes'
     ),
     new Data_Model(
-      'Volleyball Club',
-      '12:00pm – 1:00pm',
-      'https://ulacit.zoom.us/j/95341672086',
+      'Feria de Emprendimientos',
+      '3:00pm – 4:00pm',
+      'https://ulacit.zoom.us/j/97042833086',
       null,
-      'sports'
+      'leadership',
+      'lunes'
     ),
     new Data_Model(
-      'Diversity Club',
-      '12:00pm – 1:00pm',
-      'https://ulacit.zoom.us/j/91819418798',
+      'International Club',
+      '4:00pm – 5:00pm',
+      'https://ulacit.zoom.us/j/92647034306',
       null,
-      'diversity'
-    ),
-    new Data_Model(
-      'Falcon Fury Marching Band',
-      '12:00pm – 1:00pm',
-      'https://ulacit.zoom.us/j/91089545841',
-      null,
-      'marching'
-    ),
-    new Data_Model(
-      'Falcon Rockapellas',
-      '1:00pm – 2:00pm',
-      'https://ulacit.zoom.us/j/96813484693',
-      null,
-      'rockapellas'
-    ),
-    new Data_Model(
-      'Innovation Club',
-      '1:00pm – 2:00pm',
-      'https://ulacit.zoom.us/j/97478728330',
-      null,
-      'innovation'
+      'international',
+      'lunes'
     ),
     new Data_Model(
       'Civic House',
-      '3:00pm – 4:00pm',
-      'https://ulacit.zoom.us/j/97222547504',
+      '5:00pm – 6:00pm',
+      'https://ulacit.zoom.us/j/92984648802',
       null,
-      'civic'
+      'civic',
+      'lunes'
+    ),
+    // Martes
+    new Data_Model(
+      'Diversity Club',
+      '10:00am – 11:00am',
+      'https://ulacit.zoom.us/j/94629344001',
+      null,
+      'diversity',
+      'martes'
     ),
     new Data_Model(
-      'Falcon D Squad',
+      'Falcon Rockapellas (Club de Canto)',
       '3:00pm – 4:00pm',
-      'https://ulacit.zoom.us/j/95746511162',
+      'https://ulacit.zoom.us/j/98860690452',
       null,
-      'falcond'
+      'rockapellas',
+      'martes'
+    ),
+    new Data_Model(
+      'Green Club',
+      '4:00pm – 5:00pm',
+      'https://ulacit.zoom.us/j/99991684374',
+      null,
+      'green',
+      'martes'
+    ),
+    new Data_Model(
+      'Innovation Club',
+      '5:00pm – 6:00pm',
+      'https://ulacit.zoom.us/j/91355034064',
+      null,
+      'innovation',
+      'martes'
+    ),
+    // Miercoles
+    new Data_Model(
+      'Agenda Wellness con Warner Rojas',
+      '2:00pm – 3:00pm',
+      'https://ulacit.zoom.us/j/95612782555',
+      null,
+      'wellness',
+      'miercoles'
+    ),
+    new Data_Model(
+      'Debate Club',
+      '3:00pm – 4:00pm',
+      'https://ulacit.zoom.us/j/91317908963',
+      null,
+      'debate',
+      'miercoles'
     ),
     new Data_Model(
       'Young Women’s Club',
       '4:00pm – 5:00pm',
-      'https://ulacit.zoom.us/j/94590107988',
+      'https://ulacit.zoom.us/j/91073421900',
       null,
-      'youngwomen'
+      'youngwomen',
+      'miercoles'
     ),
+    new Data_Model(
+      'Falcon D Squad (Club de Baile)',
+      '5:00pm – 6:00pm',
+      'https://ulacit.zoom.us/j/97028444602',
+      null,
+      'falcond',
+      'miercoles'
+    ),
+    // Jueves
+    new Data_Model(
+      'English Club',
+      '12:00pm – 1:00pm',
+      'https://ulacit.zoom.us/j/95242271127',
+      null,
+      'english',
+      'jueves'
+    ),
+    new Data_Model(
+      'Book Club',
+      '2:00pm – 3:00pm',
+      'https://ulacit.zoom.us/j/95264263399',
+      null,
+      'leadership',
+      'jueves'
+    ),
+    new Data_Model(
+      'Falcon Fury Marching Band',
+      '3:00pm – 4:00pm',
+      'https://ulacit.zoom.us/j/97919978529',
+      null,
+      'marching',
+      'jueves'
+    ),
+    new Data_Model(
+      'The Great 28 (Club de Audiovisuales)',
+      '4:00pm – 5:00pm',
+      'https://ulacit.zoom.us/j/93083633000',
+      null,
+      'great28',
+      'jueves'
+    ),
+    // Viernes
     new Data_Model(
       'Soccer Club',
-      '4:00pm – 5:00pm',
-      'https://ulacit.zoom.us/j/95098592737',
+      '2:00pm – 3:00pm',
+      'https://ulacit.zoom.us/j/91557549525',
       null,
-      'sports'
+      'sports',
+      'viernes'
     ),
     new Data_Model(
-      'The Great 28',
-      '4:00pm – 5:00pm',
-      'https://ulacit.zoom.us/j/96887447942',
+      'Volleyball Club',
+      '3:00pm – 4:00pm',
+      'https://ulacit.zoom.us/j/92396533382',
       null,
-      'great28'
+      'sports',
+      'viernes'
+    ),
+    new Data_Model(
+      'Traspaso de Poderes GOES',
+      '4:00pm – 5:00pm',
+      'https://ulacit.zoom.us/j/99326777853',
+      null,
+      'leadership',
+      'viernes'
+    ),
+    new Data_Model(
+      'Concierto de Bienveniida',
+      '5:00pm – 6:00pm',
+      'https://ulacit.zoom.us/j/99326777853',
+      null,
+      'leadership',
+      'viernes'
     ),
   ]);
   let [place, setPlace] = useState('list');
@@ -153,11 +214,55 @@ function App() {
             </a>
           </div>
         </div>
-        <h2 class="principal-grid-date">Miercoles 27 de Enero 2021</h2>
+        <h2 class="principal-grid-date">Lunes 17 de Mayo 2021</h2>
         <div class="principal-grid-list">
-          {meetings.map((data, i) => (
-            <Button action={selectOption} indexObject={i} {...data} />
-          ))}
+          {meetings.map((data, i) =>
+            data.day == 'lunes' ? (
+              <Button action={selectOption} indexObject={i} {...data} />
+            ) : (
+              ''
+            )
+          )}
+        </div>
+        <h2 class="principal-grid-date">Martes 18 de Mayo 2021</h2>
+        <div class="principal-grid-list">
+          {meetings.map((data, i) =>
+            data.day == 'martes' ? (
+              <Button action={selectOption} indexObject={i} {...data} />
+            ) : (
+              ''
+            )
+          )}
+        </div>
+        <h2 class="principal-grid-date">Miercoles 19 de Mayo 2021</h2>
+        <div class="principal-grid-list">
+          {meetings.map((data, i) =>
+            data.day == 'miercoles' ? (
+              <Button action={selectOption} indexObject={i} {...data} />
+            ) : (
+              ''
+            )
+          )}
+        </div>
+        <h2 class="principal-grid-date">Jueves 20 de Mayo 2021</h2>
+        <div class="principal-grid-list">
+          {meetings.map((data, i) =>
+            data.day == 'jueves' ? (
+              <Button action={selectOption} indexObject={i} {...data} />
+            ) : (
+              ''
+            )
+          )}
+        </div>
+        <h2 class="principal-grid-date">Viernes 21 de Mayo 2021</h2>
+        <div class="principal-grid-list">
+          {meetings.map((data, i) =>
+            data.day == 'viernes' ? (
+              <Button action={selectOption} indexObject={i} {...data} />
+            ) : (
+              ''
+            )
+          )}
         </div>
       </div>
     );
--- src/data-model.js
+++ src/data-model.js
@@ -1,10 +1,11 @@
 class Data_Model {
-  constructor(name, schedule, meeting_link, video_link, logo_url) {
+  constructor(name, schedule, meeting_link, video_link, logo_url, day) {
     this.name = name;
     this.schedule = schedule;
     this.meeting_link = meeting_link;
     this.video_link = video_link;
     this.logo_url = logo_url;
+    this.day = day;
   }
 }
 
